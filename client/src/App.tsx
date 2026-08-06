227cded8e6a1b3b18ffe7669da2feeae5ff5426f
Jorell Relleve <jorellrelleve@gmail.com>
2026-08-06 17:09:37 +0800

    make a set-up for frontend

---
+++ client/src/App.tsx
@@ -0,0 +1,15 @@
+import MainPage from "@/pages/MainPage";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<MainPage />}>
+        </Route>
+      </Routes>
+    </Router>
+  )
+}
+
+export default App;
