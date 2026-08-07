2e4b13483a229caaa67c49dc78303ed58783a8f1
Jorell Relleve <jorellrelleve@gmail.com>
2026-08-07 21:43:27 +0800

    feat: add validation in task dialog

--- client/src/components/TaskDialog.tsx
+++ client/src/components/TaskDialog.tsx
@@ -1,17 +1,11 @@
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogHeader,
-  DialogTitle,
-  DialogFooter,  
-  DialogClose, 
-} from "@/components/ui/dialog";
+import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogFooter, DialogClose, } from "@/components/ui/dialog";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { Textarea } from "@/components/ui/textarea";
 import { Label } from "@/components/ui/label";
 import type { Dispatch, SetStateAction } from "react";
+import { useForm, type SubmitHandler } from "react-hook-form";
+import { useCallback } from "react";
 
 interface DialogProps {
   open: boolean;
@@ -20,39 +14,86 @@ interface DialogProps {
   isUpdate?: boolean;
 }
 
+interface IAddTask extends Pick<Task, "title" | "description"> {};
+
 export default function TaskDialog (props: DialogProps) {
   const { open, id, onOpenChange, isUpdate = false } = props;
+
+  const {
+    register,
+    reset,
+    handleSubmit,
+    formState: { isSubmitting, errors },
+  } = useForm<IAddTask>({
+    mode: "onChange", // Needed for onChange validation so error message would appear
+  });
+
+  // Submit handler
+  const onSubmit: SubmitHandler<IAddTask> = useCallback(
+    async (data) => {
+      console.log(data)
+      /* reset();
+      queryClient.invalidateQueries({ queryKey: ["admin-records"], });
+      onOpenChange(false); */
+    },
+    [onOpenChange], // add deps
+  );
+
   return (
-  <Dialog open={open} onOpenChange={onOpenChange}>
-    <DialogContent>
-      <DialogHeader className="text-left">
-        <DialogTitle>{isUpdate ? "Update Task" : "Add Task"}</DialogTitle>
-         <DialogDescription>
-          {isUpdate 
-            ? "Make changes to the task title, and description" 
-           : "Fill in the details to add a new task to your board."
-          }
-         </DialogDescription>
-      </DialogHeader>
-      <div className="flex flex-col gap-y-3 [&_label]:text-sm mb-2 [&_label]:text-gray-700">
-         <div className="space-y-2">  <Label htmlFor="title">
-             Title
-           </Label>
-           <Input placeholder="Enter task title" id="title" />
-        </div>
-        <div className="space-y-2">  <Label htmlFor="description">
-             Description
-           </Label>
-           <Textarea placeholder="Enter task description" id="description" rows={4} />
-        </div> 
-      </div>
-      <DialogFooter className="flex flex-row justify-end gap-x-3">
-         <DialogClose>
-           <Button variant="outline" type="button" className="rounded-lg">Close</Button>
-         </DialogClose>
-         <Button className="rounded-lg">{isUpdate ? "Save Changes" : "Save Task"}</Button>
-       </DialogFooter>
-    </DialogContent>
-  </Dialog> 
+    <Dialog open={open} onOpenChange={onOpenChange}>
+      <DialogContent>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <DialogHeader className="text-left">
+            <DialogTitle>{isUpdate? "Update Task" : "Add Task"}</DialogTitle>
+            <DialogDescription>
+              {isUpdate
+               ? "Make changes to the task title, and description"
+                : "Fill in the details to add a new task to your board."
+              }
+            </DialogDescription>
+          </DialogHeader>
+
+          <div className="flex flex-col gap-y-3 [&_label]:text-sm mb-2 [&_label]:text-gray-700">
+            <div className="space-y-2">
+              <Label htmlFor="title">Title</Label>
+              <Input
+                placeholder="Enter task title"
+                id="title"
+                {...register("title", { required: "Task title is required" })}
+              />
+              {errors.title && <p className="text-red-500 text-xs">{errors.title.message}</p>} {/* Show error */}
+            </div>
+
+            <div className="space-y-2">
+              <Label htmlFor="description">Description</Label>
+              <Textarea
+                placeholder="Enter task description"
+                id="description"
+                rows={4}
+                {...register("description", { required: "Description is required" })}
+              />
+              {errors.description && <p className="text-red-500 text-xs">{errors.description.message}</p>} {/* Show error */}
+            </div>
+          </div>
+
+          <DialogFooter className="flex flex-row justify-end gap-x-3">
+            <DialogClose asChild>
+              <Button
+                variant="outline"
+                type="button"
+                className="rounded-lg"
+                onClick={() => reset()} // reset on close
+              >
+                Close
+              </Button>
+            </DialogClose>
+            <Button type="submit" className="rounded-lg" disabled={isSubmitting}>
+              {/* type submit */}
+              {isSubmitting ? "Saving..." : (isUpdate ? "Save Changes" : "Save Task")}
+            </Button>
+          </DialogFooter>
+        </form>
+      </DialogContent>
+    </Dialog>
   )
 };
